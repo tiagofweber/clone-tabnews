4434471dc35e1c19c316c97fd81dd712a0144c87
Tiago Fernando Weber <74274609+tiagofweber@users.noreply.github.com>
2024-04-14 22:18:30 +0000

    Modifica a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mariana, eu amo você!</h1>
+    return <h1>Mariana, saiba que sou muito feliz com você e com a nossa família!<br></br> Com amor, Tiago.</h1>
 }
 
 export default Home;
