ae51ab8f4986c2bc122b1fd750d56cca7c512bf7
Tiago Fernando Weber <74274609+tiagofweber@users.noreply.github.com>
2024-04-18 22:44:46 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do projeto https://www.tabnews.com.br com o objetivo de estudo de programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Mariana, saiba que sou muito feliz com você e com a nossa família!<br></br> Com amor, Tiago.</h1>
+  return (
+    <h1>
+      Mariana, saiba que sou muito feliz com você e com a nossa família!
+      <br></br> Com amor, Tiago.
+    </h1>
+  );
 }
 
 export default Home;
